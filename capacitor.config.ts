cd0f83b750457932ed631d81d8b5490fdc569987
dimitarradulov <76252856+dimitarradulov@users.noreply.github.com>
2026-06-16 20:02:35 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'solo-wrestler',
+  webDir: 'www'
+};
+
+export default config;
